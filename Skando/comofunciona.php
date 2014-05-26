e5e8961d4ef04d63836ec52cf4b08daf8e0a4f3e
Coopermine <david@directip.com.br>
2014-05-26 15:53:01 +0000

    Commit inicial

---
+++ Skando/comofunciona.php
@@ -0,0 +1,74 @@
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<?php
+require 'fbconfig.php';   // Include fbconfig.php file
+
+?>
+<!doctype html>
+
+<html xmlns:fb="http://www.facebook.com/2008/fbml">
+
+  <head>
+ <link href="css/bootstrap.min.css" rel="stylesheet"> 
+</head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <body>
+<div class="panel panel-primary">
+  <div class="panel-heading"><center>Skando</center></div>
+  <div class="panel-body">
+
+
+<div class="container" id="tourpackages-carousel">
+      
+      <div class="row">
+        
+        <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="http://thelittlehawk.com/wp-content/uploads/2011/02/wifi-logo4.jpg" alt="">
+              <div class="caption">
+                <h4>Sem acesso?</h4>
+                <p>Cansado de perguntar a senha wifi dos locais que você mais gosta de frequentar? O Skando é a solução para seus problemas. Veja como é simples usar.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="http://blog.marketwired.com/wp-content/uploads/2013/11/facebook_twitter-500x300.jpg" alt="">
+              <div class="caption">
+                <h4>Redes sociais</h4>
+                <p>Use seu facebook ou Twitter para logar na internet, sem precisar de senhas. Encontre pessoas que gostam de frequentar os mesmos locais que você.</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-18 col-sm-6 col-md-3">
+          <div class="thumbnail">
+            <img src="promocao.jpg" alt="">
+              <div class="caption">
+                <h4>Promoções exclusivas</h4>
+                <p>Cada vez que você acessa, acumula pontos que podem ser trocados por vários premios. Ta esperando o que? Começe a acumular seus pontos.</p>
+            </div>
+          </div>
+        </div>
+      
+	</div><!-- End row -->
+      
+    </div><!-- End container -->
+
+<center><center>
+<div>
+<a href="<?php echo "index.php"; ?>">
+<button type="button" class="btn btn-default btn-lg" id="liberar2" data-loading-text="Processando...">
+<span class="glyphicon glyphicon-thumbs-up"></span> QUERO USAR AGORA!
+</button>
+</center>
+</a>
+
+
+
+<br>
+</div>
+</div>
+</div>
+  </body>
+</html>
---
+++ fbconfig.php
@@ -0,0 +1,32 @@
+<?php
+require 'src/facebook.php';  // Include facebook SDK file
+//require 'functions.php';  // Include functions
+$facebook = new Facebook(array(
+  'appId'  => '1497359610483109',   // Facebook App ID 
+  'secret' => '80b89eaf4a173e4d957b7e52171ad232',  // Facebook App Secret
+  'cookie' => true,	
+));
+$user = $facebook->getUser();
+if ($user) {
+  try {
+    $user_profile = $facebook->api('/me');
+  	    $fbid = $user_profile['id'];                 // To Get Facebook ID
+ 	    $fbuname = $user_profile['username'];  // To Get Facebook Username
+ 	    $fbfullname = $user_profile['name']; // To Get Facebook full name
+	      $femail = $user_profile['email'];    // To Get Facebook email ID
+    //       checkuser($fbid,$fbuname,$fbfullname,$femail);    // To update local DB
+  } catch (FacebookApiException $e) {
+    error_log($e);
+   $user = null;
+  }
+}
+if ($user) {
+  $logoutUrl = $facebook->getLogoutUrl(array(
+		 'next' => 'http://azuredirectip.cloudapp.net/bot/logout.php',  // Logout URL full path
+		));
+} else {
+ $loginUrl = $facebook->getLoginUrl(array(
+		'scope'		=> 'email', // Permissions to request from the user
+		));
+}
+?>
